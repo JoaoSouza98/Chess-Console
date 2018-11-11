f4d238aefa4c175cfd7ac64015252a4ac898a17c
Joao Souza <joao_suza@outlook.com>
2018-11-11 10:09:32 -0200

    Create class Posicao

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using board;
 
 namespace chess_console {
     class Program {
         static void Main(string[] args) {
+
+            Position p;
+
+            p = new Position(3, 4);
+
+            Console.WriteLine("Position: " + 
+                p);
+
+            Console.ReadLine();
+
         }
     }
 }
