64d3405bd44cdddd971d60658474dfe403789aee
Joao Souza <joao_suza@outlook.com>
2018-11-11 13:34:53 -0200

    Create class ChessPosition

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -5,18 +5,10 @@ using chess;
 namespace chess_console {
     class Program {
         static void Main(string[] args) {
-            try {
-                Board b = new Board(8, 8);
 
-                b.putPiece(new Rook(b, Color.Black), new Position(0, 0));
-                b.putPiece(new King(b, Color.Black), new Position(0, 2));
-                b.putPiece(new Rook(b, Color.Black), new Position(1, 9));
-                b.putPiece(new King(b, Color.Black), new Position(2, 4));
+            ChessPosition pos = new ChessPosition('c', 7);
 
-                Screen.showBoard(b);
-            } catch (BoardException e) {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos.toPosition());
 
             Console.ReadLine();
 
