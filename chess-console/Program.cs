590415a051155d310a81b7bed5c74cd2ca33720a
Joao Souza <joao_suza@outlook.com>
2018-11-11 12:52:31 -0200

    Handling errors with custom exceptions

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -5,14 +5,18 @@ using chess;
 namespace chess_console {
     class Program {
         static void Main(string[] args) {
+            try {
+                Board b = new Board(8, 8);
 
-            Board b = new Board(8, 8);
+                b.putPiece(new Rook(b, Color.Black), new Position(0, 0));
+                b.putPiece(new King(b, Color.Black), new Position(0, 2));
+                b.putPiece(new Rook(b, Color.Black), new Position(1, 9));
+                b.putPiece(new King(b, Color.Black), new Position(2, 4));
 
-            b.putPiece(new Rook(b, Color.Black), new Position(0, 0));
-            b.putPiece(new Rook(b, Color.Black), new Position(1, 3));
-            b.putPiece(new King(b, Color.Black), new Position(2, 4));
-
-            Screen.showBoard(b);
+                Screen.showBoard(b);
+            } catch (BoardException e) {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadLine();
 
--- chess-console/board/Board.cs
+++ chess-console/board/Board.cs
@@ -15,9 +15,38 @@
             return pieces[row, column];
         }
 
+        //override improvment for piece positioning
+        public Piece piece(Position pos) {
+            return pieces[pos.row, pos.column];
+        }
+
+        public bool IsTherePiece(Position pos) {
+            validatePosition(pos);
+
+            return piece(pos) != null;
+        }
+
         public void putPiece(Piece p, Position pos) {
+            if (IsTherePiece(pos)) {
+                throw new BoardException("There's already a piece at this position!");
+            }
             pieces[pos.row, pos.column] = p;
             p.position = pos;
+        }
+
+        //catch invalid position according to the board size
+        public bool validPosition(Position pos) {
+            if (pos.row < 0 || pos.row >= rowsAmount || pos.column < 0 || pos.column >= columnsAmount) {
+                return false;
+            }
+            return true;
+        }
+
+        //case validPosition == false, throw an customized Exception
+        public void validatePosition(Position pos) {
+            if(!validPosition(pos)) {
+                throw new BoardException("Invalid position!");
+            }
         }
     }
 }
